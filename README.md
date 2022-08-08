e85b80acf9983b515c8928015ea9a90acfce5a1c
Kevin SG <kevin.boster99@gmail.com>
2022-08-08 13:16:15 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NOTES APP - FIREBASE
+
+## Technologies
+
+- React.js
+- Typescript
+- Firebase
+
+## How To Use
+
+To clone and run this application, you'll need Git and Node.js (which comes with yarn) installed on your computer. From your command line:
+
+```bash
+# Clone this repository
+$ git clone https://github.com/kevin-sg/note-firebase-app.git
+
+# Install dependencies
+$ yarn install
+
+# Run the app
+$ yarn dev
+```
---
+++ tailwind.config.cjs
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', 'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#f0fdfa',
+          100: '#ccfbf1',
+          200: '#99f6e4',
+          300: '#5eead4',
+          400: '#2dd4bf',
+          500: '#14b8a6',
+          600: '#0d9488',
+          700: '#0f766e',
+          800: '#115e59',
+          900: '#134e4a',
+        },
+      },
+    },
+    fontFamily: {
+      custom: ['Roboto', 'Poppins', ...defaultTheme.fontFamily.sans],
+    },
+  },
+};
