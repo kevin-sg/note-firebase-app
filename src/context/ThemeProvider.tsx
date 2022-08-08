e17376a230bf84408c990f0cc34b101fd1b1ad06
Kevin SG <kevin.boster99@gmail.com>
2022-08-08 13:33:03 -0300

    Added context

---
+++ src/context/ThemeProvider.tsx
@@ -0,0 +1,18 @@
+import { createContext, useMemo, useState, ReactNode, Dispatch, SetStateAction, useEffect } from 'react';
+
+import { initialTheme } from '@/utilities';
+
+interface IThemeProviderProps {
+  theme: string;
+  setTheme: Dispatch<SetStateAction<string>>;
+}
+
+export const ThemeContext = createContext({} as IThemeProviderProps);
+
+export const ThemeProvider = ({ children }: { children: ReactNode }) => {
+  const [theme, setTheme] = useState(initialTheme);
+
+  const values = useMemo(() => ({ theme, setTheme }), []);
+
+  return <ThemeContext.Provider value={values}>{children}</ThemeContext.Provider>;
+};
