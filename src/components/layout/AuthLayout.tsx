ecce06d464259ee209abff16751cc6a206d76346
Kevin SG <kevin.boster99@gmail.com>
2022-08-08 13:27:44 -0300

    Added layout

---
+++ src/components/layout/AuthLayout.tsx
@@ -0,0 +1,32 @@
+import type { ReactNode, FC } from 'react';
+
+import { FaReact } from 'react-icons/fa';
+
+interface IAuthLayoutProps {
+  title: string;
+  children: ReactNode;
+}
+
+const AuthLayout: FC<IAuthLayoutProps> = ({ title, children }) => {
+  return (
+    <section className='bg-gray-50 dark:bg-gray-900'>
+      <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
+        <span className='flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white'>
+          <FaReact className='mr-2 text-blue-500 text-3xl' />
+          Preview Page
+        </span>
+        <div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
+          <div className='p-6 space-y-4 sm:p-8'>
+            <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-whit'>
+              {title}
+            </h1>
+
+            {children}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AuthLayout;
