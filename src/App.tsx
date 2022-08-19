7730396488269421d45b5f0731e45fdf50a0dcaf
Kevin SG <kevin.boster99@gmail.com>
2022-08-19 09:38:07 -0300

    Add Redux provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import type { FC } from 'react';
 
+import { store } from '@/redux';
 import { RootRouter } from '@/routers';
 import { ThemeProvider } from '@/context';
 
 const App: FC = () => {
   return (
-    <ThemeProvider>
-      <BrowserRouter>
-        <RootRouter />
-      </BrowserRouter>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider>
+        <BrowserRouter>
+          <RootRouter />
+        </BrowserRouter>
+      </ThemeProvider>
+    </Provider>
   );
 };
 
