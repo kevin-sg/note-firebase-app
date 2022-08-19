0781e8edb4b78b34b0d4f39cd64820a0e15532ee
Kevin SG <kevin.boster99@gmail.com>
2022-08-19 09:44:59 -0300

    Updated configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import * as path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import * as path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+  server: {
+    host: true,
+    port: 3000,
   },
 });
